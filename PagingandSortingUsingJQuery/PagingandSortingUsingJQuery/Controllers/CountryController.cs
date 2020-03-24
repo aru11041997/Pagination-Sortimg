00f03dbdc4bda21d8d587d9f94389a144cb6c06b
Anand Uperpelli <anand_uperpelli@epam.com>
2020-03-24 12:53:45 +0530

    Done

---
+++ PagingandSortingUsingJQuery/PagingandSortingUsingJQuery/Controllers/CountryController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagingandSortingUsingJQuery.Models;
+
+namespace PagingandSortingUsingJQuery.Controllers
+{
+    public class CountryController : Controller
+    {
+        // GET: Country
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetList()
+        {
+            DBModel dBModel = new DBModel();
+            var countryList = dBModel.countries.ToList<country>();
+            return Json(new { data = countryList }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
